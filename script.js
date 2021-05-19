fb383975b547f9fb903d0fc0eba27cdffbace729
JornMeessen <jornmeessen@hotmail.com>
2021-05-19 21:29:01 +0200

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+var buttonBestellen = document.getElementById('button_bestellen');
+var buttonBereiden = document.getElementById('button_bereiden');
+var buttonBezorgen = document.getElementById('button_bezorgen');
+var video = document.getElementById('video');
+
+
+buttonBestellen.addEventListener('click', speelBestellen);
+buttonBereiden.addEventListener('click', speelBereiden);
+buttonBezorgen.addEventListener('click', speelBezorgen);
+
+function speelBestellen() {
+    video.src = "pizzeria_files/bestellen.mp4";
+    console.log('run bestellen');
+}
+
+function speelBereiden() {
+    video.src = "pizzeria_files/bereiden.mp4";
+    console.log('run bereiden');
+}
+
+function speelBezorgen() {
+    video.src = "pizzeria_files/bezorgen.mp4";
+    console.log('run bezorgen');
+}
